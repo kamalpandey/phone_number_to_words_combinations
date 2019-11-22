acab46c5852465fb08a8d9dbabb66d2d1b369718
Kamal Pandey <kpandeyy@gmail.com>
2019-11-22 09:41:20 +0545

    Added specs for finding all possible combinations'

---
+++ spec/combinations.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+describe PossibleCombinations do
+  let(:phone) { '6686787825' }
+  let(:min_length) { 3 }
+  let(:max_length) { 10 }
+
+  it 'should returns all possible combinations for given phone number' do
+    combinations = PossibleCombinations
+                   .number_of_possible_combinations(phone,
+                                                    min_length,
+                                                    max_length)
+    expect(combinations).to match_array([%w[6686787825],
+                                         %w[668 6787825],
+                                         %w[6686 787825],
+                                         %w[66867 87825],
+                                         %w[668678 7825],
+                                         %w[6686787 825],
+                                         %w[678 7825 668],
+                                         %w[7825 668 678],
+                                         %w[668 678 7825]])
+  end
+end
